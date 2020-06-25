56fcfdc58a5c930d49574d8b21198ba59dcae56d
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-06-25 00:00:13 -0400

    frame set

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Header from "./Header";
+
+const Layout = (props) => {
+  return (
+    <div>
+      <Head>
+        <title>TKB Custom Design</title>
+        <link
+          rel="stylesheet"
+          href="https://bootswatch.com/4/minty/bootstrap.min.css"
+        />
+      </Head>
+      <Header />
+      {props.children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const Index = () => {
+  return <Layout>Hello World!</Layout>;
+};
+
+export default Index;
